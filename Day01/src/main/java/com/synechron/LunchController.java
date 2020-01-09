6503bbfc61119c2b238dd74fc38fb91352c67421
prabhu-durasoft <iamprabhu@yahoo.com>
2020-01-09 12:39:51 +0530

    Day01

--- Day01/src/main/java/com/synechron/LunchController.java
+++ Day01/src/main/java/com/synechron/LunchController.java
@@ -4,14 +4,24 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LunchController {
 
+	@Value("${lunch.message}")
+	private String welcome;
+	
 	@Autowired
 	private List<String> lunchOptions;
+	
+	@GetMapping("/lunch/welcome")
+	public String getWelcomeMessage() {
+		return welcome;
+	}
+	
 	
 	@GetMapping("/lunch")
 	public List<String> getMenuItems() {
